78ed226a472fa06bc0c0935e83d1f77f603f6ae3
Noah Sicklick <noahsicklick@gmail.com>
2019-10-23 12:29:21 -0500

    Initial framework for sentence vector. Probably need to find a larger corpus to train our data

---
+++ sentenceTokenizer/sentenceToke.py
@@ -0,0 +1,52 @@
+import os
+import gensim
+import smart_open
+import collections
+import random
+
+test_data_dir = os.path.join(gensim.__path__[0], 'test', 'test_data')
+lee_train_file = os.path.join(test_data_dir, 'lee_background.cor')
+lee_test_file = os.path.join(test_data_dir, 'lee.cor')
+
+
+def read_corpus(fname, tokens_only=False):
+    with smart_open.open(fname, encoding="iso-8859-1") as f:
+        for i, line in enumerate(f):
+            tokens = gensim.utils.simple_preprocess(line)
+            if tokens_only:
+                yield tokens
+            else:
+                # For training data, add tags
+                yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+
+
+train_corpus = list(read_corpus(lee_train_file))
+test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
+
+model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+model.build_vocab(train_corpus)
+model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+vector = model.infer_vector(['only', 'you', 'can', 'prevent', 'forest', 'fires'])
+#print(vector)
+
+ranks = []
+second_ranks = []
+for doc_id in range(len(train_corpus)):
+    inferred_vector = model.infer_vector(train_corpus[doc_id].words)
+    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+    rank = [docid for docid, sim in sims].index(doc_id)
+    ranks.append(rank)
+
+    second_ranks.append(sims[1])
+
+
+counter = collections.Counter(ranks)
+doc_id = random.randint(0, len(test_corpus) - 1)
+inferred_vector = model.infer_vector(test_corpus[doc_id])
+sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+
+# Compare and print the most/median/least similar documents from the train corpus
+print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(test_corpus[doc_id])))
+print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
+for label, index in [('MOST', 0), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
+    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
