0e9a4d32c5dd4b1072db6a7e72ce534d74bc92f1
Dillon O'Leary <doleary@wisc.edu>
2019-10-24 18:29:38 -0500

    Nothing really

--- extension/test/test.js
+++ extension/test/test.js
@@ -1,8 +1,14 @@
 var assert = require('assert');
+var sum = require('../sum');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
+});
+
+describe('Input interface', function() {
+	describe('#')
+	it('should ')
 });
