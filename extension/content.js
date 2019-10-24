3a457da49abd160b1f105fba255a40257ca22d63
Vinay <vjanard@gmail.com>
2019-10-24 17:25:54 -0500

    Commented out proof of concept script

--- extension/content.js
+++ extension/content.js
@@ -1,2 +1,2 @@
-let jQueryTest = $("a[href^='http']").eq(0).attr("href");
-console.log("DeepCite says: ", jQueryTest);
+// let jQueryTest = $("a[href^='http']").eq(0).attr("href");
+// console.log("DeepCite says: ", jQueryTest);
