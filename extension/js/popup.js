8388dbf1353be7298fc5115aa178373a74907cb0
Vinay <vjanard@gmail.com>
2019-10-24 21:06:15 -0500

    Sends form data successfully

---
+++ extension/js/popup.js
@@ -0,0 +1,30 @@
+let url = "http://localhost:3000"
+
+
+$(document).ready(() => {
+    //data = {claim:"abc", link:"asdf"}; //dummy data
+    //$("#btnsubmit").click(data, sendToServer);
+    $('#linxerForm').on('submit', (event) => {
+        event.preventDefault();
+        data = {
+                claim:event.target["0"].value, 
+                link:event.target["1"].value
+                };
+        sendToServer(data); //perform some operations
+    });
+})
+
+
+function sendToServer(data){
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: dataReceived,
+        contentType: "application/json",
+        data: JSON.stringify(data)
+    })
+}
+
+function dataReceived(data){
+    console.log("Received: ", data);
+}
