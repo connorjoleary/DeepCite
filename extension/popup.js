90a1bb760a4103e168522b6309e81b6d7bd5602d
Dillon O'Leary <doleary@wisc.edu>
2019-10-24 19:53:27 -0500

    Before I figure out how to run js

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,16 @@
-let url = "127.0.0.1"
-
-
+const url = "127.0.0.1:3000"
+const bkg = chrome.extension.getBackgroundPage();
+bkg.console.log("sdassdgsafdff");
+document.getElementById("title").innerHTML = "TESTTE";
 $(document).ready(() => {
     $("submit").click(data, sendToServer);
+    console.log("sdf");
+    bkg.console.log("sdasdff");
 })
 
 
 function sendToServer(data){
+	bkg.console.log("data sent to server");
     $.ajax({
         type: "POST",
         url: url,
