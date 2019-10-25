148c794b31f3b8594738f44149eb0e9a4416af79
Vinay <vjanard@gmail.com>
2019-10-24 20:23:49 -0500

    Added test webserver which mirrors JSONs

---
+++ test-server/README.txt
@@ -0,0 +1,7 @@
+To install:
+npm install
+
+To run:
+npm start
+
+This is a local webserver running on port 3000 which returns any JSON files it receives
---
+++ test-server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.send('Send me a JSON!');
+});
+
+app.post('/', (req,res) => {
+    res.send(req.body);
+    console.log(req.body);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
