ced56482bea9371b815b6e3362c76ed657e617a4
Dillon O'Leary <doleary@wisc.edu>
2019-10-24 20:29:58 -0500

    nothing important

--- extension/popup.js
+++ extension/popup.js
@@ -1,25 +1,36 @@
 const url = "127.0.0.1:3000"
-const bkg = chrome.extension.getBackgroundPage();
-bkg.console.log("sdassdgsafdff");
-document.getElementById("title").innerHTML = "TESTTE";
-$(document).ready(() => {
-    $("submit").click(data, sendToServer);
-    console.log("sdf");
-    bkg.console.log("sdasdff");
-})
+// $(document).ready(() => {
+//     $("submit").click(data, sendToServer);
+//     console.log("sdf");
+// })
 
 
-function sendToServer(data){
-	bkg.console.log("data sent to server");
-    $.ajax({
-        type: "POST",
-        url: url,
-        success: dataReceived,
-        dataType: "json",
-        data: data
-    })
+// function sendToServer(data){
+// 	bkg.console.log("data sent to server");
+//     $.ajax({
+//         type: "POST",
+//         url: url,
+//         success: dataReceived,
+//         dataType: "json",
+//         data: data
+//     })
+// }
+
+
+
+// function dataReceived(data){
+//     console.log(data);
+// }
+
+function main() {
+	document.getElementById("title").innerHTML = "TESTTE";
 }
 
-function dataReceived(data){
-    console.log(data);
-}
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+  // document.querySelector('button').addEventListener('click', clickHandler);
+  console.log("sdf");
+  main();
+});
