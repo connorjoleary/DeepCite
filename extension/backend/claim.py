482a41dcf759b6474f8f85d20e7c405e0137ba75
juliajyh <47750603+juliajyh@users.noreply.github.com>
2019-10-30 19:41:17 -0500

    Add files via upload
    update claim to test

--- extension/backend/claim.py
+++ extension/backend/claim.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import tokenizer
 import sys
 import io
 
-
-class Claim(nn.Module):
-    def __init__(self, href, text):
+class Claim:
+    def __init__(self, href, text, order):
         super(Claim, self).__init__()
         # hrefs : several reference links, which is a list of str
         # text : the text of the claim
@@ -14,6 +14,7 @@ class Claim(nn.Module):
         self.href = href
         self.text = text
         self.child = []
+        self.branch = order
         # default value of score is 0
         self.score = 0 
 
@@ -25,10 +26,20 @@ class Claim(nn.Module):
         for unit in text_raw:
             if len(unit.findAll('a')) > 0:
                 for ref in unit.findAll('a'):
-                    ref2text [ref['href']] = unit.text
-        cand = select_origin(self, ref2text)
-        for link, text in cand.items():
-            self.child.append(Claim(link, text))
+                    ref2text[unit.text] = ref['href']
+        cand = tokenizer.predict(self.text, ref2text.keys, self.branch)
+        for text in cand:
+            self.child.append(Claim(ref2text[text], text, self.order))
         return self.child
+
+if "__main__":
+    
+    url = "http://math.ucr.edu/home/baez/physics/Relativity/GR/grav_speed.html"
+    text = "In the simple newtonian model, gravity propagates instantaneously: the force exerted by a massive object points directly toward that object's present position.  For example, even though the Sun is 500 light seconds from the Earth, newtonian gravity describes a force on Earth directed towards the Sun's position "
+    root = Claim(url, text, 5)
+    print(root.parse_child())
+
+
+
         
 
