4a6277be904d5265fb7a2213759f7c2c6c846947
juliajyh <47750603+juliajyh@users.noreply.github.com>
2019-11-10 17:41:04 -0600

    Add files via upload
    Update Scraper!

--- backend/nlp/advanced_scraper.py
+++ backend/nlp/advanced_scraper.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-#import tokenizer
+import tokenizer
 import sys
 import io
 
@@ -12,39 +12,48 @@ class Claim:
         # text : the text of the claim
         # child: a list of claims
         # score: matching score computed by nlp algorithm. Will be edited by user response.
+        # parent: a instance of Claim class.
+        # visited: a list to store all the hrefs for cycle detection.
         self.href = href
         self.text = text
         self.parent = parent
         self.child = []
         self.height = height
-        self.childtext = self.parse_child(maxheight)
+        self.leaf = {}
+        self.visited = []
+        self.cand, self.score = self.parse_child(maxheight)
         
         # self.branch = order
         # default value of score is 0
 
     def parse_child(self, maxheight):
+        if self.parent != None:
+            self.visited = self.parent.visited
         ref2text = {}
-        print(self.href, self.height)
-        if self.href[:5] != "https":
-            #print(self.parent)
-            #print(self.parent.split('/'))
-            preref = "https://" + self.parent.split('/')[2]
+        if self.href[:5] != "https" and self.parent != None:
+            preref = "https://" + self.parent.href.split('/')[2]
             print(preref)
             self.href = "".join([preref, self.href])
+        # Cycle Detection
+        if self.href in self.visited:
+            # Terminate the scraper and parse the parent node to the leaf list
+            self.leaf[self.parent.texts[0]] = self.parent.scores[0]
+            return self.parent.texts[0], self.parent.scores[0]
         response = requests.get(self.href)
+        self.visited.append(self.href)
         soup = BeautifulSoup(response.text, 'html.parser')
         text_raw = soup.findAll('p')
-        child_text = []
+        # Exception that the child of one claim has no valid sentences, then add its parent to the leaf list.
+        if len(text_raw) < 5:
+            # Terminate the scraper and parse the parent node to the leaf list
+            self.leaf[self.parent.texts[0]] = self.parent.scores[0]
+            return self.parent.texts[0], self.parent.scores[0]
         for unit in text_raw:
             if len(unit.findAll('a')) > 0:
                 for ref in unit.findAll('a'):
                     ref2text[unit.text] = ref['href']
-        for text in ref2text.keys(): 
-            #if self.height < maxheight:
-            self.child.append(Claim(ref2text[text], text, (self.height +1), self.href))
-        for child in self.child:
-            child_text.append(child.text)
-        '''
+            else:
+                ref2text [unit.text] = ""
         cand = tokenizer.predict(self.text, list(ref2text.keys()), 1)
         texts = [] 
         scores = []
@@ -52,36 +61,33 @@ class Claim:
             try:
                 if ref2text[text[1]] != "" and self.height < maxheight:
                     print(ref2text[text[1]])
-                    self.child.append(Claim(ref2text[text[1]], text[1], (self.height + 1)))
+                    self.child.append(Claim(ref2text[text[1]], text[1], (self.height + 1), self))
             except KeyError:
                 ref_key = ""
                 for key in ref2text.keys():
                     if text[1] in key:
                         ref_key = key
                         break
+                print(ref2text[ref_key])
                 if ref2text[ref_key] != "" and self.height < maxheight:
-                    self.child.append(Claim(ref2text[text[1]], text[1], (self.height +1)))
-                    print(ref2text[text[1]])
+                    texts.append(text[1])
+                    scores.append(text[2])
+                    self.child.append(Claim(ref2text[text[1]], text[1], (self.height +1), self))
+                elif ref2text[ref_key] == "":
+                    self.leaf[text[1]] = text[2]
+            
+        return texts, scores
 
-            texts.append(text[1])
-            scores.append(text[2])
-        return texts, scores'''
-        return child_text
 
-    def cycledetection():
-        return 0
-        
-
-   # def __repr__(self):
-   #     return "claim: " + self.text + " text: " + str(self.childtext)
-        
+    def __repr__(self):
+        return "claim: " + self.text + "leaves of the citation tree: " + self.leaf
 
 if "__main__":
     
-    url = "https://www.britannica.com/biography/Aristotle"
-    text = "Draconian laws are named after the 1st Greek legislator, Draco, who meted out severe punishment for very minor offenses. These included enforced slavery for any debtor whose status was lower than that of his creditor and the death sentence for stealing a cabbage."
-    root = Claim(url, text, 0, "")
-    print(str(root.child))
+    url = "http://math.ucr.edu/home/baez/physics/Relativity/GR/grav_speed.html"
+    text = "Gravity moves at the Speed of Light and is not Instantaneous. If the Sun were to disappear, we would continue our elliptical orbit for an additional 8 minutes and 20 seconds, the same time it would take us to stop seeing the light (according to General Relativity)."
+    root = Claim(url, text, 0, None)
+
 
 
 
