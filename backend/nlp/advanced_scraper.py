b60ec1be06fe0602c3efd07b4952c288ef292908
juliajyh <47750603+juliajyh@users.noreply.github.com>
2019-11-10 22:54:12 -0600

    Add files via upload
    A bug-free scraper! Need more test on corner cases

--- backend/nlp/advanced_scraper.py
+++ backend/nlp/advanced_scraper.py
@@ -5,8 +5,8 @@ import sys
 import io
 
 class Claim:
-    def __init__(self, href, text, height, parent):
-        maxheight = 0 # TODO: iteration 2 problem
+    def __init__(self, href, text, height, parent, maxheight = 8):
+         # TODO: iteration 2 problem
         super(Claim, self).__init__()
         # hrefs : several reference links, which is a list of str
         # text : the text of the claim
@@ -21,33 +21,53 @@ class Claim:
         self.height = height
         self.leaf = {}
         self.visited = []
-        self.cand, self.score = self.parse_child(maxheight)
-        
+        self.parse_child(maxheight)
+        self.realscore = 0
+        # Add field cand and score to the parent so that children can work correctly
         # self.branch = order
         # default value of score is 0
+    
+    
 
-    def parse_child(self, maxheight):
+    def excep_handle(self):
         if self.parent != None:
             self.visited = self.parent.visited
-        ref2text = {}
+            self.realscore = self.parent.score
+
+        
+        # fix broken link
         if self.href[:5] != "https" and self.parent != None:
             preref = "https://" + self.parent.href.split('/')[2]
-            print(preref)
+            #print(preref)
             self.href = "".join([preref, self.href])
+            
+        
         # Cycle Detection
-        if self.href in self.visited:
+        if self.href in self.visited and self.parent != None:
             # Terminate the scraper and parse the parent node to the leaf list
-            self.leaf[self.parent.texts[0]] = self.parent.scores[0]
-            return self.parent.texts[0], self.parent.scores[0]
+            self.leaf[self.parent.text[0]] = self.parent.score[0]
+            return False
+        
+        return True
+
+    
+    def parse_child(self, maxheight):
+        # Do nothing if there is a cycle
+        ref2text = {}
+        if not self.excep_handle():
+            return 
+        
         response = requests.get(self.href)
         self.visited.append(self.href)
         soup = BeautifulSoup(response.text, 'html.parser')
         text_raw = soup.findAll('p')
-        # Exception that the child of one claim has no valid sentences, then add its parent to the leaf list.
+
+         # Exception that the child of one claim has no valid sentences, then add its parent to the leaf list.
         if len(text_raw) < 5:
             # Terminate the scraper and parse the parent node to the leaf list
-            self.leaf[self.parent.texts[0]] = self.parent.scores[0]
-            return self.parent.texts[0], self.parent.scores[0]
+            self.leaf[self.parent.text] = self.parent.score
+            return 
+
         for unit in text_raw:
             if len(unit.findAll('a')) > 0:
                 for ref in unit.findAll('a'):
@@ -60,27 +80,42 @@ class Claim:
         for text in cand:
             try:
                 if ref2text[text[1]] != "" and self.height < maxheight:
-                    print(ref2text[text[1]])
-                    self.child.append(Claim(ref2text[text[1]], text[1], (self.height + 1), self))
+                    texts.append(text[1])
+                    scores.append(text[2])
             except KeyError:
                 ref_key = ""
                 for key in ref2text.keys():
                     if text[1] in key:
                         ref_key = key
                         break
-                print(ref2text[ref_key])
+                #print(ref2text[ref_key])
                 if ref2text[ref_key] != "" and self.height < maxheight:
                     texts.append(text[1])
                     scores.append(text[2])
-                    self.child.append(Claim(ref2text[text[1]], text[1], (self.height +1), self))
-                elif ref2text[ref_key] == "":
+                elif self.height < maxheight:
                     self.leaf[text[1]] = text[2]
-            
-        return texts, scores
+                    return
+        print("text: {}".format(texts))
+        print("scores: {}".format(scores))
+        self.text = texts
+        self.score = scores
+        for words in texts:
+            try:
+                if ref2text[text[1]] != "" and self.height < maxheight:
+                    self.child.append(Claim(ref2text[words], words, (self.height +1), self))
+            except KeyError:
+                ref_key = ""
+                for key in ref2text.keys():
+                    if text[1] in key:
+                        ref_key = key
+                        break
+                if ref2text[ref_key] != "" and self.height < maxheight:
+                    self.child.append(Claim(ref2text[words], words, (self.height +1), self))
 
 
     def __repr__(self):
-        return "claim: " + self.text + "leaves of the citation tree: " + self.leaf
+        for keys, values in self.leaf.items():
+            return "claim: " + self.text + "keys of leaves: " + keys + "values of leaves: " + values
 
 if "__main__":
     
