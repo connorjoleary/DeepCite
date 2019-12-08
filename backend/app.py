3b39b92771a4d32c0c2e059208762342c9c4b19c
Catherine Yan <52057120+Chyan214@users.noreply.github.com>
2019-12-07 18:39:42 -0600

    fix tree before app.py

--- backend/app.py
+++ backend/app.py
@@ -35,8 +35,8 @@ def deep_cite():
 
     try:
         tree = Tree(link, claim)
-        print(tree.get_best_path())
-        full_pre_json = {'results': tree.get_best_path()}
+        # print()
+        # full_pre_json = {'results': tree.get_best_path()}
     except Exception as e:
         full_pre_json['error'] = str(e)
     
