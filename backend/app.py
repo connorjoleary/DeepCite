6ed2c0353d7fc2ea2b4bd52c0fdbeeaf26552e49
Catherine Yan <52057120+Chyan214@users.noreply.github.com>
2019-12-08 23:45:34 -0600

    hid under the table during the demo

--- backend/app.py
+++ backend/app.py
@@ -59,4 +59,4 @@ def sanitize_link(link):
     return sanitized.strip()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,5 +1,9 @@
-const url = "http://3.17.173.207:5000/api/v1/deep_cite";
-// const url = "http://localhost:3000";
+const url = "http://18.223.108.40:5000/api/v1/deep_cite";
+
+/*const url = "http://localhost:5000/api/v1/deep_cite";
+const host = "localhost";
+const port = 5000;
+const path = "/api/v1/deep_cite";*/
 
 
 
@@ -59,7 +63,6 @@ $(document).ready(() => {
     });
 })
 
-
 function sendToServer(data) {
     $.ajax({
         type: "POST",
@@ -67,24 +70,69 @@ function sendToServer(data) {
         success: dataReceived, // callback function on success
         contentType: "application/json", // exprected data type of the returned data
         data: JSON.stringify(data) // send the data json as a string
-    })
+    });
+
+    /*http.get({
+        host: host, 
+        port: port,
+        path: path 
+      }, function(res) {
+        $.ajax({
+            type: "POST",
+            url: url, // where the post request gets sent to (backend server address)
+            success: dataReceived, // callback function on success
+            contentType: "application/json", // exprected data type of the returned data
+            data: JSON.stringify(data) // send the data json as a string
+        })
+
+      }).on("error", function(e) {
+        $("body").html(`<div id="results" class="container-fluid main">
+                        <h2 id="title" style="font-family: Book Antiqua">Error</h1>
+                        </div>`);
+
+        $('#results').append(`
+            <div class="error">
+                <p class="result-text" style="color:#8b0000">${"Error 503: Unable to contect to server"}</p>
+            </div>
+        `);
+      });*/
 }
 
 function dataReceived(data) {
-    // update popup with results
-    console.log("Received: ", data);
-    $("body").html(`<div id="results" class="container-fluid main">
-                    <h2 id="title">Citation List</h1>
-                    </div>`);
-    //for each item in data returned:
-    data.results.forEach(result => {
-        $("#results").append(`
-        <div class="result">
-            <p class="result-text">"${result.source}"</p>
-            <a href="${result.link}" class="card-link"><b>${result.link}</b></a>
-        </div>
+
+    //prints errors:
+    if(data.error) {
+        console.log("Received: ", data);
+        $("body").html(`<div id="results" class="container-fluid main">
+                        <h2 id="title" style="font-family: Book Antiqua">Error</h1>
+                        </div>`);
+
+        $('#results').append(`
+            <div class="error">
+                <p class="result-text" style="color:#8b0000">${data.error}</p>
+            </div>
         `);
-    });
+    }
+    else{ 
+
+        // update popup with results
+        console.log("Received: ", data);
+        $("body").html(`<div id="results" class="container-fluid main">
+                        <h2 id="title" style="font-family: Book Antiqua">Citation List</h1>
+                        </div>`);
+        
+        //for each item in data returned:
+        data.results.forEach(result => {
+            $("#results").append(`
+            <div class="result">
+                <p class="result-text">"${result.source}"</p>
+                <a href="${result.link}" class="card-link"><b>${result.link}</b></a>
+            </div>
+            `);
+        });
+
+    }
+
 
     //makes anchor tags open in new tab
     $('body').on('click', 'a', function () {
