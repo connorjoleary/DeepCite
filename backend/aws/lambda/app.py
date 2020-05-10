774eca99e514663163620a32855186a972660ba6
connorjoleary <oleary.connor24@gmail.com>
2020-05-09 20:28:10 -0600

    call ecs instance from lambda

--- backend/aws/lambda/app.py
+++ backend/aws/lambda/app.py
@@ -6,4 +6,34 @@ def handler(event, context):
     claim = event['claim']
     link = event['link']
 
-    return {'status': 200, 'message': "still in progress"}
+    params = {'taskDefinition': 'deepcite',
+            'cluster': 'arn:aws:ecs:us-east-2:072491736148:cluster/deepcite',
+            'launchType': 'FARGATE',
+            'networkConfiguration':
+                {'awsvpcConfiguration': 
+                    {'subnets': [
+                        'subnet-05452e5640c015db2',
+                    ],
+                    'securityGroups': [
+                        'sg-0d1b015575624679e',
+                    ],
+                    'assignPublicIp': 'DISABLED'}
+            },
+            'overrides':
+                {'containerOverrides': [
+                    {'name': 'deepcite', 'environment': [
+                            {'name': 'claim', 'value': claim
+                            },
+                            {'name': 'link', 'value': link
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+
+    print(client.list_clusters())
+    print()
+    response = client.run_task(**params)
+    print(response)
+    return {'status': 200, 'response': "in progress"}
