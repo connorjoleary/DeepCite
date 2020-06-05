87481417063424469b351454523bc0aa281b5ab3
connorjoleary <oleary.connor24@gmail.com>
2020-06-05 15:59:49 -0600

    delete unused code comment

--- backend/aws/lambda/app.py
+++ backend/aws/lambda/app.py
@@ -36,16 +36,6 @@ def call_deepcite(claim, link):
     
 
 def lambda_handler(event, context):
-    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
-    access to the request and response payload, including headers and
-    status code.
-
-    To scan a DynamoDB table, make a GET request with the TableName as a
-    query string parameter. To put, update, or delete an item, make a POST,
-    PUT, or DELETE request respectively, passing in the payload to the
-    DynamoDB API as a JSON body.
-    '''
-    #print("Received event: " + json.dumps(event, indent=2))
 
     operations = {
         'POST': lambda x: call_deepcite(**x)
