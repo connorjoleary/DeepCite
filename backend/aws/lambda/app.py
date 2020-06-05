887d9dd474c4d6a3d365830e816700d569dbb6d4
connorjoleary <oleary.connor24@gmail.com>
2020-06-05 15:58:19 -0600

    Fix connection and add RDS connection

--- backend/aws/lambda/app.py
+++ backend/aws/lambda/app.py
@@ -1,10 +1,25 @@
 import json
 import requests
+import psycopg2
+import sys
+
+rds_host  = "deepcite.ckbyp3nhsmiu.us-east-2.rds.amazonaws.com"
+db_name = 'postgres'
+db_password = 'deepcite'
+db_username = 'postgres'
 
 print('Loading function')
-# -H "Content-Type: application/json"
-url = 'http://18.217.10.151:8000/api/v1/deep_cite'
 
+# private ip address of ec2
+url = 'http://172.31.35.42:8000/api/v1/deep_cite'
+
+try:
+    conn = psycopg2.connect(host=rds_host, user=db_username, password=db_password, database=db_name, port='5432')
+except psycopg2.OperationalError as e:
+    print("ERROR: Unexpected error: Could not connect to database instance.")
+    print(e)
+    sys.exit()
+print("SUCCESS: Connection to RDS instance succeeded")
 
 def respond(err, res=None):
 
@@ -43,9 +58,4 @@ def lambda_handler(event, context):
     else:
         return respond(ValueError('Unsupported method "{}"'.format(operation)))
 
-# curl -d '{"claim":"the death of Sherlock Holmes almost destroyed the magazine that had originally published the stories. When Arthur Conan Doyle killed him off in 1893, 20,000 people cancelled their subscriptions. The magazine barely survived. Its staff referred to Holmes’ death as “the dreadful event”."
-# , "link":"http://www.bbc.com/culture/story/20160106-how-sherlock-holmes-changed-the-world"}' 
-# -H "Content-Type: application/json" 
-# -X POST http://3.19.211.248:5000/api/v1/deep_cite
-
-print(lambda_handler({"body": "{\"claim\": \"the death of Sherlock Holmes almost destroyed the magazine that had originally published the stories. When Arthur Conan Doyle killed him off in 1893, 20,000 people cancelled their subscriptions. The magazine barely survived. Its staff referred to Holmes’ death as “the dreadful event\",\"link\": \"http://www.bbc.com/culture/story/20160106-how-sherlock-holmes-changed-the-world\"}","httpMethod": "POST"},0))
+# print(lambda_handler({"body": "{\"claim\": \"the death of Sherlock Holmes almost destroyed the magazine that had originally published the stories. When Arthur Conan Doyle killed him off in 1893, 20,000 people cancelled their subscriptions. The magazine barely survived. Its staff referred to Holmes’ death as “the dreadful event\",\"link\": \"http://www.bbc.com/culture/story/20160106-how-sherlock-holmes-changed-the-world\"}","httpMethod": "POST"},0))
