b5c9c6a6db738008c5ee2543a5379d7590f6f305
connorjoleary <oleary.connor24@gmail.com>
2020-06-13 21:26:33 -0600

    add secret and not working rds connection

--- backend/aws/lambda/app.py
+++ backend/aws/lambda/app.py
@@ -1,31 +1,67 @@
 import json
 import requests
-import psycopg2
 import sys
 import base64
 from dataclasses import dataclass
 import time
+import boto3
+from botocore.exceptions import ClientError
 
 # TODO: These should be env variables or imported from something better
 versions = {'model': 0.1, 'lambda': 0.1, 'api': 0.1, 'extension': 0.1}
-
-rds_host  = "deepcite.ckbyp3nhsmiu.us-east-2.rds.amazonaws.com"
+secret_name = "rds_deepcite_sample"
+region_name = "us-east-2"
 db_name = 'postgres'
-db_password = 'deepcite'
-db_username = 'postgres'
+
+# Create a Secrets Manager client
+session = boto3.session.Session()
+secret_client = session.client(
+    service_name='secretsmanager',
+    region_name=region_name
+)
+rds_client = session.client(
+    service_name='rds-data',
+    region_name=region_name
+)
 
 print('Loading function')
 
 # private ip address of ec2
 url = 'http://172.31.35.42:8000/api/v1/deep_cite'
 
-try:
-    conn = psycopg2.connect(host=rds_host, user=db_username, password=db_password, database=db_name, port='5432')
-except psycopg2.OperationalError as e:
-    print("ERROR: Unexpected error: Could not connect to database instance.")
-    print(e)
-    sys.exit()
-print("SUCCESS: Connection to RDS instance succeeded")
+
+def get_secret():
+    print('grabbing secret')
+    try:
+        get_secret_value_response = secret_client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            print(e)
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            print(e)
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            print(e)
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            print(e)
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            print(e)
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+    print(secret)
+    return secret
+    
 
 # need this so that I can pass json or a response object to be returned
 @dataclass
@@ -59,22 +95,35 @@ def lambda_handler(event, context):
     }
     operation = event['requestContext']['http']['method']
     
-    # print(event)
+    print(event)
     stage = event['requestContext']['stage']
 
     if operation in operations:
-        payload = load_payload(event)
-        response = respond(None, operations[operation](payload))
+        try:
+            payload = load_payload(event)
+            response = respond(None, operations[operation](payload))
+        except Exception as e:
+            print(e)
     else:
         response = respond(ValueError('Unsupported method "{}"'.format(operation)))
 
     user_id = event['requestContext']['http']['sourceIp']
     time_elapsed = time.time()-start
-    
-    cur = conn.cursor()
-    cur.execute("INSERT INTO deepcite_call (user_id,stage,status_code,response,response_time_elapsed,current_versions) VALUES (%s, %s, %s, %s, %s, %s)", (user_id,stage,response['statusCode'],response['body'],time_elapsed,json.dumps(versions)))
-    conn.commit()
-
+    secret = get_secret()
+    print('got here')
+    print("INSERT INTO deepcite_call (user_id,stage,status_code,response,response_time_elapsed,current_versions) VALUES ('{}', '{}', {}, '{}', {}, '{}')".format(user_id,stage,response['statusCode'],response['body'].replace("'", ""),time_elapsed,json.dumps(versions)))
+    try:
+        rds_response = rds_client.execute_statement(
+            resourceArn ='arn:aws:rds:us-east-2:072491736148:db:deepcite',
+            # database=db_name,
+            secretArn='arn:aws:secretsmanager:us-east-2:072491736148:secret:rds_deepcite_sample-Y9B0Sa',
+            sql ="INSERT INTO deepcite_call (user_id,stage,status_code,response,response_time_elapsed,current_versions) VALUES ('{}', '{}', {}, '{}', {}, '{}')".format(user_id,stage,response['statusCode'],response['body'].replace("'", ""),time_elapsed,json.dumps(versions))
+        )
+        print(rds_response)
+    except Exception as e:
+        print(e)
+    print(response)
+        
     return response
 
 # print(lambda_handler({"requestContext": {"http": {"method": "GET"}}},0))
---
+++ backend/aws/rds/select_recent.pgsql
@@ -0,0 +1,1 @@
+SELECT * FROM "deepcite_call" ORDER by "created_at" DESC LIMIT 1000;
